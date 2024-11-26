498426e171e380aaeeb2bf49392c470119aa2b9b
oscarleivab <oleibie2005@g.educaand.es>
2024-11-26 13:53:38 +0100

    Initial commit

---
+++ app/src/main/java/com/example/aniverse/CreditActivity.kt
@@ -0,0 +1,26 @@
+package com.example.aniverse
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CreditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_credit)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val nombre = intent.getStringExtra("EXTRA_TEXTO") ?: "Desconocido"
+        val textView2 = findViewById<TextView>(R.id.textView2)
+
+        textView2.text = "$nombre estás usando la versión 1 de AniVerse"
+
+    }
+}
---
+++ app/src/main/java/com/example/aniverse/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.aniverse
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button = findViewById<Button>(R.id.button)
+
+        val editTextText = findViewById<EditText>(R.id.editTextText)
+
+        button.setOnClickListener{
+            val texto = editTextText.text.toString()
+            val intent = Intent(this, CreditActivity::class.java)
+            intent.putExtra("EXTRA_TEXTO", texto)
+            startActivity(intent)
+        }
+
+    }
+}
