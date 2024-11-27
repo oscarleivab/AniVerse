d86f08897563a4c03857263921ed49554565e7c2
oscarleivab <oleibie2005@g.educaand.es>
2024-11-27 14:05:17 +0100

    segundo commit

---
+++ app/src/main/java/com/example/aniverse/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.example.aniverse
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
